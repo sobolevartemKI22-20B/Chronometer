7650ab742ed9132f18dfb9e2696dac788ea222c2
sobolevartemKI22-20B <artsob_2004@mail.ru>
2026-09-15 17:09:38 +0700

    Added start function

--- app/src/main/java/com/example/chronometer/MainActivity.kt
+++ app/src/main/java/com/example/chronometer/MainActivity.kt
@@ -15,6 +15,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Button
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
+import androidx.compose.runtime.mutableStateOf
 
 class MainActivity : ComponentActivity() {
 
@@ -32,6 +40,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ChronometerScreen() {
 
+    var elapsedTime by remember {
+        mutableLongStateOf(0L)
+    }
+
+    var isRunning by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(isRunning) {
+
+        while (isRunning) {
+
+            delay(1000)
+
+            elapsedTime += 1000
+        }
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -48,8 +74,33 @@ fun ChronometerScreen() {
         )
 
         Text(
-            text = "00:00",
+            text = formatTime(elapsedTime),
             fontSize = 48.sp
         )
+
+        Spacer(
+            modifier = Modifier.height(30.dp)
+        )
+
+        Button(
+            onClick = {
+                isRunning = true
+            }
+        ) {
+            Text("Start")
+        }
     }
+}
+fun formatTime(milliseconds: Long): String {
+
+    val totalSeconds = milliseconds / 1000
+
+    val minutes = totalSeconds / 60
+    val seconds = totalSeconds % 60
+
+    return String.format(
+        "%02d:%02d",
+        minutes,
+        seconds
+    )
 }
